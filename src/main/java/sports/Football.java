e5f492ea63bb1377a422f8c182841aeb0658718f
Ilgiz <kurmanaliev.ilgiz@gmail.com>
2024-08-11 23:58:02 +0600

    Baseball

--- src/main/java/sports/Football.java
+++ src/main/java/sports/Football.java
@@ -3,5 +3,7 @@ package sports;
 public class Football {
     public static void main(String[] args) {
         System.out.println("Football");
+        System.out.println("Soccer");
+        System.out.println("Soccer 2");
     }
 }
